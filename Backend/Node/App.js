300740a6a6e3969ce0ec4a459d73d0f0b24bb63f
lennikorhonen <lenni.korhonen2@gmail.com>
2021-03-02 15:55:56 +0200

    delete test added

--- Backend/Node/App.js
+++ Backend/Node/App.js
@@ -37,15 +37,11 @@ app.get('/', function (req, res) { // function to get data from test.json file
 app.get('/projects', db.getProjects)
 app.get('/info/:project', db.getProjectSoftwareInfo)
 app.get('/eolinfo', db.getEol)
-app.get('/testing', db.getEolTest)
 app.post('/software', db.createSoftware)
 app.post('/projectsoftware', db.createProjectSoftware)
 app.post('/eol', db.createEol)
 app.put('/software/:id', db.updateSoftware)
 app.delete('/software/:id', db.deleteSoftware)
-
-app.post('/python', python.runPython)
-
 
 //At the moment doesn't do anything else but logs the messages to console when using with curl
 /*
--- Backend/Node/queries.js
+++ Backend/Node/queries.js
@@ -1,4 +1,5 @@
 const dotenv = require('dotenv/config')
+const pgp = require('pg-promise')({ capSQL: true });
 
 const Pool = require("pg").Pool
 const pool = new Pool({
@@ -21,7 +22,6 @@ const getProjects = (request, response) => {
 
 // Gets project software info by project name
 const getProjectSoftwareInfo = (request, response) => {
-    console.log(request)
     const project = request.params.project
 
     pool.query('SELECT project.name, software.name, project_software.installed_version, software.latest_version FROM project_software INNER JOIN project ON project_software.project_id = project.project_id INNER JOIN software ON project_software.software_id = software.software_id WHERE project_software.project_id = (SELECT project_id FROM project WHERE name = $1)', [project], (error, results) => {
@@ -118,10 +118,23 @@ const deleteSoftware = (request, response) => {
 }
 
 const createEol = (request, response) => {
-    const { software, version, eol } = request.body
+    // const { software, version, eol } = request.body
 
-    let software_name = software.toLowerCase().replace(/\s/g, '')
-    pool.query('INSERT INTO eol (software_name, version, eol_date) VALUES ($1, $2, $3)', [software_name, version, eol], (error, results) => {
+    // let software_name = software.toLowerCase().replace(/\s/g, '')
+
+    const cs = new pgp.helpers.ColumnSet(['software_name', 'version', 'eol_date'], { table: 'eol' });
+
+    // data input values:
+    let ist = JSON.stringify(request.body.softwareList);
+    let software_list = JSON.parse(ist)
+    // generating a multi-row insert query:
+    console.log(software_list)
+    const sql = pgp.helpers.insert(software_list, cs);
+    // //=> INSERT INTO "tmp"("col_a","col_b") VALUES('a1','b1'),('a2','b2')
+
+    console.log(sql)
+
+    pool.query(sql, (error, results) => {
         if (error) {
             throw error
         }
@@ -129,15 +142,6 @@ const createEol = (request, response) => {
         response.status(201).send(JSON.stringify(results.rows[0])
 
         )
-    })
-}
-
-const getEolTest = (request, response) => {
-    pool.query('SELECT * FROM eol', (error, results) => {
-        if (error) {
-            throw error
-        }
-        response.status(200).json(results.rows)
     })
 }
 
@@ -208,6 +212,5 @@ module.exports = {
     testCon,
     createProjectSoftware,
     getEol,
-    getEolTest,
     createEol
 }
