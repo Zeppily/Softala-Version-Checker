1ad886a0b8936c3ac375ffc0ec27c2b5c50bdd3c
HiMyNameIsSunny <thaigrrs@gmail.com>
2021-03-19 09:14:59 +0200

    Refactored test to assert with lowercase

--- py_scantool/tests/test_scrapper.py
+++ py_scantool/tests/test_scrapper.py
@@ -3,7 +3,7 @@ import jsonscrape
 def test_jsonscrap():
     result = jsonscrape.scrap_eol()["softwareList"]
 
-    line_to_compare = list(filter(lambda software: software['software_name'] == 'Python', result))[0]
+    line_to_compare = list(filter(lambda software: software['software_name'] == 'python', result))[0]
     assert len(result) > 0
-    assert ("Python" == line_to_compare["software_name"])
+    assert ("python" == line_to_compare["software_name"])
     
